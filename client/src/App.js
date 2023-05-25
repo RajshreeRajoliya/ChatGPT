617973e78472694d12e3e135f8d5287cfeb6a21f
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-25 13:41:42 +0530

    added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,23 @@
 
 
 const App = () =>{
+
+  const getMessages=async()=>{
+    const options = {
+      method : 'POST',
+      body : JSON.stringify({
+        message : "hello how are you?"
+      }),
+      headers : {
+        "Content-Type": "application/json"
+      }
+    }
+try{
+await fetch('http://localhost:8000/completions' , options)
+} catch(err) {
+  console.log(err)
+}
+  }
   return (
     <div className="app">
      <section className="side-bar">
@@ -19,7 +36,7 @@ const App = () =>{
       <div className="bottom-section">
         <div className="input-container">
           <input/>
-          <div id="submit">*</div>
+          <div id="submit" onClick={getMessages}>*</div>
         </div>
         <p className="info">Chat GPT Mar 14 Version. Free Research Preview.
 Our goal is to make AI systems more natural and safe to interact with.

82c3f0397057b283d55f103eed2701bea241955d
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-25 12:46:55 +0530

    CompletedUI

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,27 @@ const App = () =>{
     <div className="app">
      <section className="side-bar">
      <button>+ New chat</button>
-     <ul className="history"></ul>
+     <ul className="history">
+      <li>ll</li>
+     </ul>
      <nav>
       <p>Made by Rajshree Rajoliya</p>
      </nav>
      </section>
 
      <section className="main">
-      
+      <h1>RajshreeGPT</h1>
+      <ul className="feed"></ul>
+      <div className="bottom-section">
+        <div className="input-container">
+          <input/>
+          <div id="submit">*</div>
+        </div>
+        <p className="info">Chat GPT Mar 14 Version. Free Research Preview.
+Our goal is to make AI systems more natural and safe to interact with.
+Your feedback wilt help us improve.
+</p>
+      </div>
      </section>
     </div>
   );
